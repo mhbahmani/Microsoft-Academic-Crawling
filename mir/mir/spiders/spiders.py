c88d40faa954497a037c16150d404aa845505306
mhbahmani <mhbahmani79@gmail.com>
2021-06-14 17:43:03 +0430

    add splash to requirements

--- mir/mir/spiders/spiders.py
+++ mir/mir/spiders/spiders.py
@@ -2,17 +2,18 @@ import scrapy
 
 class ArticlesSpider(scrapy.Spider):
     name = "articlespider"
-    start_urls = ['https://academic.microsoft.com/paper/2981549002']
+    # start_urls = ['https://academic.microsoft.com/paper/2981549002']
         #https://academic.microsoft.com/paper/3105081694
         #https://academic.microsoft.com/paper/2950893734
         #https://academic.microsoft.com/paper/3119786062
         #https://academic.microsoft.com/paper/2145339207
         #https://academic.microsoft.com/paper/2153579005']
+    start_urls = ['https://www.zyte.com/blog/page/2']
 
     def parse(self, response):
         page = response.url
         print(page)
-        filename = 'article-%s.html' % page.splite('/')[-1]
+        filename = 'article-%s.html' % page.split('/')[-2]
         with open(filename, 'wb') as f:
             f.write(response.body)
 
