c52613f6f3c4fc9c26c51fddc24403fa7e01e631
mhbahmani <mhbahmani79@gmail.com>
2021-06-16 14:23:16 +0430

    fix title

--- mir/mir/spiders/spiders.py
+++ mir/mir/spiders/spiders.py
@@ -2,43 +2,37 @@ from scrapy_splash import SplashRequest
 from bs4 import BeautifulSoup
 import scrapy
 import json
+import re
 
 class ArticlesSpider(scrapy.Spider):
     name = "articlespider"
-    start_urls = ['https://academic.microsoft.com/paper/2981549002']
-        #https://academic.microsoft.com/paper/3105081694
-        #https://academic.microsoft.com/paper/2950893734
-        #https://academic.microsoft.com/paper/3119786062
-        #https://academic.microsoft.com/paper/2145339207
-        #https://academic.microsoft.com/paper/2153579005']
+    start_urls = ['https://academic.microsoft.com/paper/2981549002',
+        'https://academic.microsoft.com/paper/3105081694',
+        'https://academic.microsoft.com/paper/2950893734',
+        'https://academic.microsoft.com/paper/3119786062',
+        'https://academic.microsoft.com/paper/2145339207',
+        'https://academic.microsoft.com/paper/2153579005']
 
     def start_requests(self): 
         for url in self.start_urls: 
             yield SplashRequest(url, self.parse, 
                 endpoint='render.html', 
-                args={'wait': 4}, 
+                args={'wait': 10}, 
            ) 
 
     def parse(self, response):
         data = {}
-
-        authors = []
-        for author in response.css('div.authors')[0].css('a::text'):
-            authors.append(author.get())
         
-        references = []
-        for ref in response.css('div.results').css('ma-card'):
-            references.append(ref.css('div.primary_paper').css('a.title.au-target').css('a[href*=paper]::attr(href)').extract()[0].split('/')[1])
-
         yield {
             'id': response.url.split('/')[-1],
             'title': BeautifulSoup(
                         response.css('h1.name').get(),
                         'html.parser'
                     ).text.strip(),
+            # 'title': re.sub('<.*>', '', str(response.css('h1.name').get())).strip(),
             'abstract': response.css('p:not(p.help-control-title.au-target)::text').get(),
             'date': response.css('span.year::text').get().strip(),
-            'authors': authors,
+            'authors': response.css('div.authors')[0].css('a::text').getall(),
             'citation_count' : \
                 response.css('div.stats'
                 ).css('ma-statistics-item'
@@ -49,7 +43,7 @@ class ArticlesSpider(scrapy.Spider):
                 ).css('ma-statistics-item'
                 )[0].css('div.count::text'
                 ).get().strip(),
-            'references': references
+            'references': [ref.split('/')[1] for ref in response.css('div.results').css('ma-card').css('div.primary_paper').css('a.title.au-target').css('a[href*=paper]::attr(href)').getall()]
         }
 
         # data['id'] = response.url.split('/')[-1]
