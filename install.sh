2cf6c108f3b59a68c3488ef1eccb4af074ba1c0c
mhbahmani <mhbahmani79@gmail.com>
2021-06-20 22:36:11 +0430

    add virtualenv

--- install.sh
+++ install.sh
@@ -33,13 +33,18 @@ main_destination=/usr/local/bin/mir_phase3
 
 rm -rf $HOME/.local/share/mir_phase3
 cd $HOME/.local/share
-git clone https://github.com/mhbahmani/Microsoft-Academic-Crawling mir_phase3
+git clone --depth=1 --branch=master https://github.com/mhbahmani/Microsoft-Academic-Crawling mir_phase3
 cd mir_phase3
+sudo rm -rf .git
 
 sudo rm -rf $main_destination 
 sudo mv mir_phase3 $main_destination
 sudo chmod +x $main_destination
 
+pip3 install virtualenv
+virtualenv venv
+
+source venv/bin/activate
 pip3 install -r requirements.txt
 rm -rf MIR_Phase3.pdf example.json README.md
 
