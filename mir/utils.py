04cac31cf03c9d2c3c127201fa578b2d89b014d9
mhbahmani <mhbahmani79@gmail.com>
2021-06-21 12:22:46 +0430

    add rank func

--- mir/utils.py
+++ mir/utils.py
@@ -7,3 +7,22 @@ def load_data(path=None):
     with open(path) as file:
         data = json.load(file)
     return [Node(article['id'], article['references']) for article in data]
+
+
+def create_authors_Rank(path=None):
+    if not path: path = 'content.json'
+
+    papers = Rank(utils.load_data)
+    with open(path) as file:
+        data = json.load(file)
+    authors = set()
+    for article in data:
+        authors.update(article['authors'])
+
+    g = {}
+    for author in authors:
+        g[author] = set()
+    for paper in papers.data:
+        for dest in paper.references:
+            node = paper.get_node(dest)
+            for authors in node.references:
